3e7739e88ba5162ad88614459a12c1839014d0af
Laura Flórez <lauraflorez520@gmail.com>
2020-03-16 23:02:07 -0500

    al fin el 8 ya está resuelto

---
+++ programacion-dos/ejercicio-8/index8.js
@@ -0,0 +1,6 @@
+"use strict";
+
+const elementosLista = document.querySelector('.lista');
+const contenido = '<li><a href="#"><img src="https://images.dog.ceo/breeds/chihuahua/n02085620_1205.jpg">Dina</a></li><li><a href="#"><img src="https://images.dog.ceo/breeds/malamute/n02110063_642.jpg">Luna</a></li><li><a href="#"><img src="https://images.dog.ceo/breeds/chihuahua/n02085620_3409.jpg">Lana</a></li>';
+
+elementosLista.innerHTML = contenido;
